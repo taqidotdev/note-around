2b7370997d4180bdaa5b6a0eba515ed5917b5762
Taqi <muhammad@taqi.dev>
2026-05-29 11:05:54 +0400

    feat: main ui
    forgot to commit since godot doesn't have an in-built system like
    vscode

---
+++ h_box_container_light.gd
@@ -0,0 +1,7 @@
+extends HBoxContainer
+
+func _ready():
+	print("heloooooo")
+
+func _on_night_mode_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
---
+++ info_modal.gd
@@ -0,0 +1,8 @@
+extends TextureRect
+
+func _on_info_pressed() -> void:
+	visible = true
+
+
+func _on_close_pressed() -> void:
+	visible = false
